aec502ec61f7c05a6a2563aa0c73eb8a0be69670
mlwmlw <520cutecat@gmail.com>
2015-10-07 00:45:21 +0800

    add file tree

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,7 @@
 var serialPort = require("serialport");
 CodeMirror.modeURL = "assets/codemirror/mode/%N/%N.js";
 var clips = [];
-angular.module('xapp', ['ngSanitize', 'hljs', 'ui.codemirror']).controller('ctrl', function($scope, $interval, $timeout) {
+angular.module('xapp', ['ngSanitize', 'hljs', 'ui.codemirror', 'treeControl']).controller('ctrl', function($scope, $interval, $timeout) {
 	var hljs = document.querySelector('.hljs')
 	var input = document.querySelector('#cmd')
 	$scope.baudrates = [
@@ -21,8 +21,26 @@ angular.module('xapp', ['ngSanitize', 'hljs', 'ui.codemirror']).controller('ctrl
 		input.focus();
 		input.selectionStart = input.selectionEnd = input.value.length;
 	};
+	$scope.treeOptions = {
+	    nodeChildren: "children",
+	    dirSelectable: true,
+	    injectClasses: {
+	        ul: "a1",
+	        li: "a2",
+	        liSelected: "a7",
+	        iExpanded: "a3",
+	        iCollapsed: "a4",
+	        iLeaf: "a5",
+	        label: "a6",
+	        labelSelected: "a8"
+	    }
+	}
+	$scope.files = [];
+	$scope.showSelected = function(node) {
+		$scope.serial.readFile(node.path, node.size)
+	}
 	$scope.input = function(event) {
-		if(!$scope.p)
+		if(!$scope.serial)
 			return;
 		//var c = String.fromCharCode(event.charCode);
 		if(event.keyCode == 38 && pointer > 0) {//up
@@ -41,33 +59,118 @@ angular.module('xapp', ['ngSanitize', 'hljs', 'ui.codemirror']).controller('ctrl
 		history.push(cmd);
 		pointer = history.length;
 		$scope.cmd = '';
-		$scope.p.write(cmd + "\n", function(err, result) {
+		$scope.serial.write(cmd + "\n", function(err, result) {
 			if(err)
 				alert(err);
 		});
 	};
 	$scope.output = '';
 	$scope.selectPort = function(com) {
-		var p = $scope.p = new serialPort.SerialPort(com, {
-			baudrate: $scope.baudrate
-		});
-		p.open(function (error) {
-			if ( error ) {
-				console.log('failed to open: '+error);
-			} else {
-				$scope.$apply(function() {
-					$scope.output = ">\n";
+		var serial = $scope.serial = new Serial(com, $scope.baudrate);
+		serial.onRead(function(data) {
+			$scope.$apply(function() {
+				$scope.output += data;
+				$timeout(function() {
+					hljs.scrollTop = hljs.scrollHeight;
 				});
-				$scope.focus();
-				p.on('data', function(data) {
-					$scope.$apply(function() {
-						$scope.output += data;
-						$timeout(function() {
-							hljs.scrollTop = hljs.scrollHeight;
-						});
+			});
+		});
+		serial.onInit(function() {
+			serial.getFiles(function(files) {
+				$scope.$apply(function() {
+					$scope.files = files.map(function(row) {
+						var split = row[0].split('/');
+						var name = split.shift();
+						var children = split.shift();
+						if(children)
+							return {name: name, children: [{
+								name: children,
+								path: name + '/' + children,
+								size: row[1]
+							}]}
+						else
+							return {name: name, path: name, size: row[1]}
 					});
 				});
-			}
+			});
+			$scope.$apply(function() {
+				$scope.output = ">\n";
+			});
 		});
 	};
+	var Serial = function(port, baudrate) {
+		var reader = function() {}, init = function() {};
+		var serial = new serialPort.SerialPort(port, {
+			baudrate: baudrate
+		});
+		var buffer = "";
+		serial.open(function (error) {
+			if ( error ) {
+				return console.log('failed to open: '+error);
+			}
+			init();
+			serial.on('data', function(data) {
+				buffer += data;
+				reader(data);
+			});
+		});
+		function execute(cmd, cb) {
+			var _r = reader;
+			var lastRead;
+			var str = "";
+			reader = function(data) {
+				lastRead = new Date;
+				str += data;
+			}
+			var timer = setInterval(function() {
+				var now = new Date;
+				if(now - lastRead < 500)
+					return;
+				reader = _r;
+				clearInterval(timer);
+				console.log(str)
+				var arr = str.toString().split("\n");
+				cb(arr.slice(cmd.split("\n").length).slice(0, -1).join("\n"))
+			}, 100);
+			var cmds = cmd.split("\n");
+			for(var i in cmds) {
+				setTimeout((function(cmd) {
+					return function() {
+						serial.write(cmd + "\n");
+					}
+				})(cmds[i]), 150 * i);
+			}
+		}
+		return {
+			readFile: function(file, size) {
+    			execute(`file.open("${file}", "r")
+    				data = file.read(${size})
+    				file.close()
+    				print(data)`, function(res) {
+					console.log(res);
+				})
+			},
+			getFiles: function(cb) {
+				execute(`l = file.list();
+					for k,v in pairs(l) do
+					  print(k..":"..v)
+					end`, function(res) {
+					cb(res.split("\n").map(function(row) {
+						return row.split(":");
+					}))
+				})
+			},
+			write: function(data, cb) {
+				serial.write(data, cb);
+			},
+			onRead: function(cb) {
+				reader = cb;
+			},
+			onInit: function(cb) {
+				init = cb;
+			}
+		}
+	};
+
+
 });
